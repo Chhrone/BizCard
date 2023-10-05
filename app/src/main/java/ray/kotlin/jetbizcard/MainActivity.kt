b0248d5236ef2666e1df706ca362b3f520548877
Chhrone <rayinailham9@gmail.com>
2023-10-05 22:09:49 +0700

    lopyuu

--- app/src/main/java/ray/kotlin/jetbizcard/MainActivity.kt
+++ app/src/main/java/ray/kotlin/jetbizcard/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,7 +20,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -31,9 +36,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -60,12 +69,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun createBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(true)
+    }
     Card(modifier = Modifier
         .fillMaxHeight()
         .padding(6.dp),
         colors = CardDefaults.cardColors(Color.White),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
-        Column(modifier = Modifier.fillMaxWidth().padding(vertical = 30.dp),
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 30.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally) {
 
@@ -78,7 +92,7 @@ fun createBizCard() {
 
             createProfileDesc()
 
-            ElevatedButton(onClick = { Log.d("Clicked","CreateBizCard: Clicked!!") },
+            ElevatedButton(onClick = { buttonClickedState.value = !buttonClickedState.value },
                 modifier = Modifier
                     .padding(10.dp)
                     .width(190.dp),
@@ -89,6 +103,55 @@ fun createBizCard() {
                     fontSize = 15.sp,
                     color = Color.White
                 )
+            }
+            if (buttonClickedState.value) {
+                createProjectBox()
+            } else {
+                Box() {}
+            }
+        }
+    }
+}
+
+@Composable
+private fun createProjectBox() {
+    Box(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxSize()
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3"))
+        }
+    }
+}
+
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data) { item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                shape = RectangleShape,
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)) {
+                    createProfileImage(modifier = Modifier
+                        .size(80.dp))
+                    Column(modifier = Modifier.padding(8.dp).align(alignment = Alignment.CenterVertically)) {
+                        Text(text = (item), fontWeight = FontWeight.Bold, fontSize = 18.sp)
+                        Text(text = "A great project befitting a great programmer!", fontWeight = FontWeight.Normal, fontSize = 14.sp)
+                    }
+
+                }
+
             }
         }
     }
@@ -120,12 +183,12 @@ private fun createProfileDesc() {
 }
 
 @Composable
-private fun createProfileImage() {
+private fun createProfileImage(modifier: Modifier = Modifier) {
     Surface(
         modifier = Modifier
             .size(150.dp)
             .padding(4.dp),
-        color = Color.White,
+        color = Color.Transparent,
         shape = CircleShape,
         shadowElevation = 4.dp,
         border = BorderStroke(0.5.dp, Color.LightGray)
